df7af00352b884ae813b14f44560b890fcd1128c
Tina Humphrey <tinahumphrey@Tinas-MacBook-Air.local>
2023-12-10 16:40:28 -0500

    commit javascript changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //Declare a variable to store the searched city
 var city = "Tampa";
 const cityInput = document.querySelector(".city-input");
-var futureWeather = document.querySelector("#future-weather");
+//var futureWeather = document.querySelector("#future-weather");
 
 // variable declaration
 var searchFormEl = document.querySelector('#search-form');
@@ -21,7 +21,7 @@ var APIKey = "e8a8374f29bc3187a7b794e86f244acd"; //The API key from OpenWeatherM
 
 // Here we build the URL so we can get a data from server side.
 //function currentWeather(city){
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 fetch(queryURL, {
     method: 'GET', //GET is the default.
     credentials: 'same-origin', // include, *same-origin, omit
@@ -55,9 +55,10 @@ fetch(queryURL, {
 // } 
 
 
-var futureWeather = function () {
-    const cityName = cityInput.value.trim();
-    if(!cityName) return;
+const futureWeather = () => {
+    const cityName = cityInput.value.trim(); // Get user entered city and remove extra spaces
+    localStorage.setItem("cityName", cityName);  // Storing City Input in Local Storage
+    if(!cityName) return;  //Return if cityName is empty
     const FiveDayAPIURL = "api.openweathermap.org/data/2.5/forecast?q=${cityName}lat={lat}&lon={lon}&appid={e8a8374f29bc3187a7b794e86f244acd}"
 
 //Get entered city from the API response
@@ -71,19 +72,45 @@ fetch(FiveDayAPIURL, {
   .then(function (data) {
     console.log(data)
   });
-
-
+}
+  var arr = [
+    {
+        temp: 44,
+        wind: "34mph",
+        humidty: 45
+    },
+    {
+        temp: 42,
+        wind: "24mph",
+        humidty: 55
+    },
+    {
+        temp: 66,
+        wind: "50mph",
+        humidty: 15
+    },
+    {
+        temp: 25,
+        wind: "77mph",
+        humidty: 95
+    },
+    {
+        temp: 34,
+        wind: "22mph",
+        humidty: 11
+    },
+]
 
 // for (let i = 0; i < 40; i+=8) { // i+=8 is going to get the data of the next day in the array of 40 objects in the five days weather response
-for (let i = 0; i < data.length; i++) { 
+for (let i = 0; i < arr.length; i++) { 
     var card = `
     <div class="card">
       <div class="card-body">
         <p id="fDate1"></p>
         <p id="fImg1"></p>
-        <p>Temp:<span id="fTemp1">${data.temp}</span></p>
-        <p>Wind:<span id="fWind1">${data.wind}</span></p>
-        <p>Humidity:<span id="fHumidity1">${data.humidty}</span></p>
+        <p>Temp:<span id="fTemp1">${arr[i].temp}</span></p>
+        <p>Wind:<span id="fWind1">${arr[i].wind}</span></p>
+        <p>Humidity:<span id="fHumidity1">${arr[i].humidty}</span></p>
       </div>
     </div>
     `
@@ -93,7 +120,7 @@ for (let i = 0; i < data.length; i++) {
     fiveDayForcast.append(newDiv);
 }
 
-}
+
 //Click Handlers
 //searchButton.addEventListener("click",displayWeather);
 $("#search-button").on("click",futureWeather);
