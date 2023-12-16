89d0159714db52d1cd4e801d0325b6ddd42eef9a
Tina Humphrey <humphrey14t@gmail.com>
2023-12-16 01:25:26 -0500

    commit javascript changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,45 @@
 // //Declare a variable to store the searched city
 var cityName = '';
-//var city = "Tampa";
-//var futureWeather = document.querySelector("#future-weather");
-var currentWeather = document.querySelector("#current-weather");
+
+
+var currentWeatherEl = document.querySelector("#current-weather");
 // variable declaration
 var searchFormEl = document.querySelector('#search-form');
-var searchCity = $("#search-city");
-var searchButton = $("#search-button");
-var currentCity = $("#current-city");
-var currentTemperature = $("#temperature");
-var currentHumidty= $("#humidity");
-var currentWind = $("#wind");
-var fiveDayForcast = $(".five-day-forcast")
-var weatherIcon = $("#weather-icon")
+var searchCity = document.querySelector("#search-city");
+var fiveDayForcast = document.querySelector(".five-day-forcast")
+// var weatherIcon = $("#weather-icon")
 
 var searchButton = document.querySelector(".search-btn");
 
-const cityInput = document.querySelector(".city-input");
+var cityInputvalue = document.querySelector(".city-input");
 var APIKey = "e8a8374f29bc3187a7b794e86f244acd"; //The API key from OpenWeatherMap 
 
 // Here we build the URL so we can get a data from server side.
-function currentWeather () {
-  //var cityName = cityInputvalue.trim(); // Get user entered city and remove extra spaces
-const WeatherMapAPIURL = `https://api.openweathermap.org/data/2.5/weather&q=` + cityName + "&appid=" + APIKey;
+function currentWeather (e) {
+  e.preventDefault()
+  var cityName = cityInputvalue.value; // Get user entered city and remove extra spaces
+  localStorage.setItem("cityName", cityName);  // Storing City Input in Local Storage
+
+const WeatherMapAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=` + cityName + "&appid=" + APIKey;
 //Get entered city from API response
 fetch(WeatherMapAPIURL,
-{
-  method: "GET", //GET is the default.
-  credentials: "same-origin", // include, *same-origin, omit
-  redirect: "follow", // manual, *follow, error
-}
+
 ).then(function (response) {
+  console.log("RES: ", response)
     return response.json();
   })
   .then(function (data) {
     console.log(data)
-
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+    futureWeather(lat, lon)
     var image = $("<img>");
     image.attr("src", `https://openweathermap.org/img/w/${data.weather[0].icon}.png`); // Weather Icon Image
-    weatherIcon.html(image)
-    currentCity.text(`${data.name}`) + dayjs().format('MM/DD/YYYY'); // Displays the City Name and JS Format
-    currentTemperature.text(`Temp: ${data.main.temp}`) // "Temp: " + data.main.temp
-    currentWind.text(`Wind: ${data.wind.deg}`) // "Wind: " + data.wind.deg
-    currentHumidty.text(`Humidity: ${data.main.humidity}`) // "Humidity: " + data.main.humidity
+    $("#weather-icon").html(image)
+    $("#current-city").text(`${data.name}`) + dayjs().format('MM/DD/YYYY'); // Displays the City Name and JS Format
+    $("#temperature").text(`Temp: ${data.main.temp}`) // "Temp: " + data.main.temp
+    $("#wind").text(`Wind: ${data.wind.deg}`) // "Wind: " + data.wind.deg
+    $("#humidity").text(`Humidity: ${data.main.humidity}`) // "Humidity: " + data.main.humidity
   });
 }
 // parse the response to display the current weather including the City name. the Date and the weather icon. 
@@ -57,75 +54,42 @@ fetch(WeatherMapAPIURL,
 // } 
 
 
-// const futureWeather = () => {
-//     const cityName = cityInput.value.trim(); // Get user entered city and remove extra spaces
-//     localStorage.setItem("cityName", cityName);  // Storing City Input in Local Storage
-//     if(!cityName) return;  //Return if cityName is empty
-//     const FiveDayAPIURL = `api.openweathermap.org/data/2.5/forecast?q=${cityName}lat={lat}&lon={lon}&appid={e8a8374f29bc3187a7b794e86f244acd}`;
+function futureWeather(lat, lon) {
 
-// //Get entered city from the API response
-// fetch(FiveDayAPIURL, {
-//     method: 'GET', //GET is the default.
-//     credentials: 'same-origin', // include, *same-origin, omit
-//     redirect: 'follow', // manual, *follow, error
-// }).then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data)
-//   });
-// }
-  var arr = [
-    {
-        temp: 44,
-        wind: "34mph",
-        humidty: 45
-    },
-    {
-        temp: 42,
-        wind: "24mph",
-        humidty: 55
-    },
-    {
-        temp: 66,
-        wind: "50mph",
-        humidty: 15
-    },
-    {
-        temp: 25,
-        wind: "77mph",
-        humidty: 95
-    },
-    {
-        temp: 34,
-        wind: "22mph",
-        humidty: 11
-    },
-]
+  const FiveDayAPIURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
 
-//for (let i = 0; i < 40; i+=8) { // i+=8 is going to get the data of the next day in the array of 40 objects in the five days weather response
-for (let i = 0; i < arr.length; i++) { 
-    var card = `
-    <div class="card">
-      <div class="card-body">
-        <p id="fDate1"></p>
-        <p id="fImg1"></p>
-        <p>Temp:<span id="fTemp1">${arr[i].temp}</span></p>
-        <p>Wind:<span id="fWind1">${arr[i].wind}</span></p>
-        <p>Humidity:<span id="fHumidity1">${arr[i].humidty}</span></p>
-      </div>
-    </div>
-    `
+  //Get entered city from the API response
+  fetch(FiveDayAPIURL
 
+  ).then(function (response) {
+      return response.json();
+  })
+  .then(function (data) {
+  
+    // i+=8 is going to get the data of the next day in the array of 40 objects in the five days weather response
+    for (let i = 0; i < data.list.length; i+=8) {
+      console.log("FIVE DAY: ", data.list[i])
+        var card = `
+          <div class="card">
+            <div class="card-body">
+              <p id="fDate1"></p>
+              <p id="fImg1"></p>
+              <p>Temp:<span id="fTemp1">${data.list[i].main.temp}</span></p>
+              <p>Wind:<span id="fWind1">${data.list[i].wind.deg}</span></p>
+              <p>Humidity:<span id="fHumidity1">${data.list[i].main.humidty}</span></p>
+            </div>
+          </div>
+          `
     var newDiv = $("<div>");
     newDiv.html(card);
     fiveDayForcast.append(newDiv);
+    }
+ });
 }
-
 
 //Click Handlers
 searchButton.addEventListener("click", currentWeather);
-//$("#search-button").on("click",futureWeather);
+
 
 
 
