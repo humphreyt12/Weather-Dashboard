2e08b51be81ef679f99cfcf45230d40bb94e7e44
Tina Humphrey <humphrey14t@gmail.com>
2024-01-03 00:10:20 -0500

    made changes to javacript file to show search history to appear as buttons and converted temp and wind speed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,27 @@
 var cityName = [];
 var citySearch = [];
 
-var currentWeatherEl = document.querySelector("#current-weather");
-// variable declaration
-var searchFormEl = document.querySelector('#search-form');
-var searchCity = document.querySelector("#search-city");
-var fiveDayForecast = document.querySelector(".five-day-forecast")
-// var weatherIcon = $("#weather-icon")
 
+// variable declaration
+var searchForm = document.querySelector('#search-form');
+var searchCity = document.querySelector("#search-city");
+var currentWeatherEl = document.querySelector("#current-weather");
+var fiveDayForecast = document.querySelector(".five-day-forecast");
 var searchButton = document.querySelector(".search-btn");
+var clearButton = $("#clear-history");
+var cityList = document.querySelector("#city-list");
 var cityInput = document.querySelector(".city-input");
+var searchContainer = document.querySelector(".search-container");
+
+// searches the city to see if it exists in the entries from the storage
+function find(c){
+  for (var i=0; i<citySearch.length; i++){
+      if(c.toUpperCase()=== citySearch[i]){
+          return -1;
+      }
+  }
+  return 1;
+}
 var APIKey = "e8a8374f29bc3187a7b794e86f244acd"; //The API key from OpenWeatherMap 
 
 
@@ -18,10 +30,14 @@ var APIKey = "e8a8374f29bc3187a7b794e86f244acd"; //The API key from OpenWeatherM
 function currentWeather (e) {
   e.preventDefault()
 
+  var searchHistory = JSON.parse(localStorage.getItem("citySearch")) || [];
 // TODO: When the user hits submit, their search is saved to local storage
  var cityName = cityInput.value.trim(); // Get user entered city and remove extra spaces
-citySearch.push(cityName);
+
+ if (searchHistory.includes(cityName) === false) {
+  citySearch.push(cityName);
  localStorage.setItem("citySearch", JSON.stringify(citySearch));
+ }
   
  localStorage.setItem("cityName", cityName);  // Storing City Name in Local Storage
 
@@ -46,12 +62,13 @@ fetch(WeatherMapAPIURL, {
     var image = $("<img>");
     image.attr("src", `https://openweathermap.org/img/w/${data.weather[0].icon}.png`); // Weather Icon Image
     $("#current-city").text(`${data.name}`) + $("#date").text(`${date}`) + $("#weather-icon").html(image)// Displays the City Name and Date in Day.JS Format
-    $("#temperature").text(`Temp: ${data.main.temp} ℉`) // "Temp: " + data.main.temp
-    $("#wind").text(`Wind: ${data.wind.deg} °`) // "Wind: " + data.wind.deg
+    $("#temperature").text(`Temp: ${((data.main.temp - 273.15) * 1.80 + 32).toFixed(2)} ℉`) // "Temp: " + data.main.temp
+    $("#wind").text(`Wind: ${(data.wind.speed*2.237).toFixed(1)} MPH`) // "Wind: " + data.wind.speed
     $("#humidity").text(`Humidity: ${data.main.humidity} %`) // "Humidity: " + data.main.humidity
   });
 }
 
+//Get future weather data from the five day API response
 function futureWeather(lat, lon) {
 
   const FiveDayAPIURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
@@ -70,54 +87,94 @@ function futureWeather(lat, lon) {
   .then(function (data) {
   
     // i+=8 is going to get the data of the next day in the array of 40 objects in the five days weather response
-    for (let i = 0; i < data.list.length; i+=8) {
+    for (let i = 0; i < data.list.length; i++) {
    
       console.log("FIVE DAY: ", data.list[i])
       const date = dayjs(data.list[i].dt_txt).format('M/D/YYYY')
-
+        // Creating the five day weather cards
         var card = `
           <div class="card">
             <div class="card-body">
               <p id="fDate1">${date}</p>
               <img src="https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png"></img>  
-              <p>Temp:<span id="fTemp1">${data.list[i].main.temp} ℉</span></p>
-              <p>Wind:<span id="fWind1">${data.list[i].wind.deg} °</span></p>
+              <p>Temp:<span id="fTemp1">${((data.list[i].main.temp - 273.15) * 1.80 + 32).toFixed(2)} ℉</span></p>
+              <p>Wind:<span id="fWind1">${(data.list[i].wind.speed*2.237).toFixed(1)} MPH</span></p>
               <p>Humidity:<span id="fHumidity1">${data.list[i].main.humidity} %</span></p>
             </div>
           </div>
           `
-   
     var newDiv = $("<div>");
     newDiv.html(card);
     fiveDayForecast.append(newDiv[0]);
-    
     }
  });
 }
 
+//Clear the search history from the page
+function clearHistory(event){
+  citySearch = [];
+  localStorage.removeItem("cityname");
+
+}
+
 //Click Handlers
 searchButton.addEventListener("click", currentWeather);
+$("#clear-history").on("click",clearHistory);
 
+  // TODO: Once that item is saved to local storage, a new button is immediately added to the page
+  function rendercityName() {
+  cityList.innerHTML = "";
+ 
+  for (var i = 0; i < cityName.length; i++) {
+    var city = cityName[i];
+
+    var li = document.createElement("li");
+    li.textContent = city;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "city";
+
+    li.appendChild(button);
+    cityList.appendChild(li);
+  } 
+}
 // TODO: On page load. Grab all items in local storage, and display buttons on screen.
 function init () {
   var storedCitySearch = JSON.parse(localStorage.getItem("citySearch"));// get local storage
-console.log(storedCitySearch);
-
-  // If city were retrieved from localStorage, update the citySearch array to it
-  if (storedCitySearch !== null) {
+   // If city were retrieved from localStorage, update the citySearch array to it
+   if (storedCitySearch !== null) {
     citySearch = storedCitySearch;
   }
-  // render buttons
-  var button = document.createElement("button");// TODO: Once that item is saved to local storage, a new button is immediately added to the page
-  button.textContent = cityName;
   
-  var li = document.createElement("li");
-  li.textContent = citySearch;
-
-  li.appendChild(button);
-  cityName.appendChild(li);
+  // render buttons
+  for (let city in citySearch) {
+    var button = document.createElement("button");
+    button.textContent = citySearch[city];
+    searchContainer.appendChild(button);
+  }
 }
-  // storeCitySearch();
-  // renderCityName();
+
+
+searchForm .addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var cityText = cityInput.value.trim();
+
+
+});
+cityList.addEventListener("click", function(event) {
+  var element = event.target;
+
+  // Checks if element is a button
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    cityName.splice(index, 1);
+    rendercityName();
+  }
+});
+
 init()
   
+
